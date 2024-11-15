05344b6dc5cadd63e9db4f247089a7e08096896f
bryancastillo10 <bryanangelo.castillo.10@gmail.com>
2024-11-15 10:31:54 +0800

    📃component: Reusable Input

---
+++ client/src/components/ui/Input.tsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { type Icon, Eye, EyeSlash } from "@phosphor-icons/react";
+
+interface InputProps {
+  id: string;
+  type?: string;
+  label: string;
+  disabled?: boolean;
+  required?: boolean;
+  value: string | number;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  isPassword?: boolean;
+  icon?: Icon;
+  validationMessage?: string;
+}
+
+const Input = ({
+  id,
+  type = "text",
+  label,
+  value,
+  onChange,
+  disabled,
+  isPassword = false,
+  required,
+  icon: Icon,
+  validationMessage,
+}: InputProps) => {
+      const [isVisible, setIsVisible] = useState<boolean>(false);
+
+      const toggleVisible = () => {
+        setIsVisible(!isVisible);
+      };
+    
+  return (
+    <div className="relative my-8">
+      <input
+        id={id}
+        type={type === "password" ? (isVisible ? "text" : "password") : type}
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+        required={required}
+        placeholder=" "
+        className={`peer w-full p-2 caret-secondary border  rounded-xl outline-none transition 
+            disabled:opacity-50 disabled:cursor-not-allowed indent-2 bg-light/20 text-dark border-dark focus:border-secondary
+            `}
+      />
+      <div
+        className="absolute z-10 top-2 left-4 origin-[0] duration-150 flex items-center gap-2
+        transform -translate-y-10 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0"
+      >
+        {Icon && <Icon size="24" color={value ? "#040710" : "#c2c2c2"} />}
+        <label
+          className={`text-lg ${
+            value ? "text-dark" : "text-neutral"
+          } transition-colors duration-150`}
+        >
+          {label}
+        </label>
+      </div>
+      {isPassword && (
+        <div
+          onClick={toggleVisible}
+          className="absolute z-10 top-2.5 right-4 cursor-pointer"
+        >
+          {isVisible ? <EyeSlash size="24" /> : <Eye size="24" />}
+        </div>
+      )}
+      {validationMessage && (
+        <div className="mt-1">
+          <p className="indent-2 text-xs">{validationMessage}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Input;
--- client/src/features/auth/components/SignIn.tsx
+++ client/src/features/auth/components/SignIn.tsx
@@ -1,15 +1,14 @@
-import Button from "@/components/ui/Button";
 
 const SignIn = () => {
+
+
   return (
     <main className="bg-primary/50 w-full h-screen">
       <section className="flex flex-col justify-center items-center h-screen w-fit m-auto">
         <div className="bg-light rounded-2xl shadow-md w-[300px] p-8">
           <h1 className="font-semibold text-2xl text-dark">Water Analytics</h1>
           <div className="mt-8">
-            <Button type="button" variant="primary" action={() => {}}>
-              Submit
-            </Button>
+
           </div>
         </div>
       </section>
