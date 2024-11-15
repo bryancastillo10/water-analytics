4a7d3633de668aa4f1d4ad546fdb17bce981e2a1
bryancastillo10 <bryanangelo.castillo.10@gmail.com>
2024-11-15 16:04:34 +0800

    ⚙️ state: set up sidebarSlice

--- client/src/lib/redux/globalReducer.ts
+++ client/src/lib/redux/globalReducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from "@reduxjs/toolkit";
-import userReducer from "./userSlice"; 
 
+import userReducer from "@/lib/redux/states/userSlice"; 
+import sidebarReducer from "@/lib/redux/states/sidebarSlice";
 
 const globalReducer = combineReducers({
   user: userReducer,
+  sidebar: sidebarReducer
 });
 
 
