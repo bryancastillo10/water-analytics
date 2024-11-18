3ea4e666c682015715c8728f6741282611a682d5
bryancastillo10 <bryanangelo.castillo.10@gmail.com>
2024-11-18 16:57:36 +0800

    🟦 layout: reset password page

--- client/src/features/auth/components/AuthContainer.tsx
+++ client/src/features/auth/components/AuthContainer.tsx
@@ -19,7 +19,11 @@ const AuthContainer = ({
           <h1 className="font-secondary text-2xl tracking-wider text-center text-nowrap text-primary mb-2">
             {title}
           </h1>
+<<<<<<< HEAD
                   <p className="text-darkGray text-center">{caption}</p>
+=======
+          <p className={`text-darkGray leading-tight ${captionAlignment}`}>{caption}</p>
+>>>>>>> c869548 (🟦 layout: reset password page)
           <hr className="border border-neutral my-4" />
           <div className="mt-4">{body}</div>
         </div>
--- client/src/features/auth/components/ForgotPassword.tsx
+++ client/src/features/auth/components/ForgotPassword.tsx
@@ -1,25 +1,38 @@
 import { useState } from "react";
+<<<<<<< HEAD
 import { Envelope } from "@phosphor-icons/react";
 import { CodeInput, FormInput, ProgressBar } from "@/components/ui";
+=======
+import { useNavigate } from "react-router-dom";
+import { Envelope, Lock, Key } from "@phosphor-icons/react";
+import { FormInput, ProgressBar, Button, CodeInput } from "@/components/ui";
+import AuthContainer from "./AuthContainer";
+>>>>>>> c869548 (🟦 layout: reset password page)
 
-// enum STEP {
-//     EMAIL = 0,
-//     VALIDATION = 1,
-//     RESETPASSWORD=2
-// }
+enum STEP {
+    EMAIL = 0,
+    VALIDATION = 1,
+    RESETPASSWORD=2
+}
 
 const ForgotPassword = () => {
-    // const [step, setStep] = useState(STEP.EMAIL);
-    const [email, setEmail] = useState<string>("");
+    const [step, setStep] = useState(STEP.EMAIL);
+    const navigate = useNavigate();
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPw, setConfirmPw] = useState<string>("");
 
+    const stepBackward = () => {
+        setStep((val) => val - 1);
+    }
 
-    // const stepBackward = () => {
-    //     s etStep((val) => val - 1);
-    // }
-
-    // const stepForward = () => {
-    //     setStep((val) => val + 1);
-    // }
+    const stepForward = () => {
+        setStep((val) => val + 1);
+    }
+  
+   const handleCompletedCode = (code:string) => {
+    console.log(code);
+    }
 
     // const actionLabel = useMemo(() => {
     //     if (step = STEP.RESETPASSWORD) {
@@ -35,6 +48,7 @@ const ForgotPassword = () => {
     //     return 'Back';
     // },[step]);
 
+<<<<<<< HEAD
     const handleCompletedCode = (code: string) => {
         console.log(typeof code);
     }
@@ -71,6 +85,116 @@ const ForgotPassword = () => {
   }
 
   return (
+=======
+  let bodyContent = (
+    <>
+      <form onSubmit={()=>{}}>
+        <FormInput
+          id="email"
+          label="Email"
+          icon={Envelope}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          validationMessage="your.email@domain.com"
+        />
+        <div className="flex items-center justify-between gap-x-2">
+          <Button action ={()=>navigate("/")} type="button" width="w-full" variant="outline">
+            Back
+          </Button>
+          <Button action={stepForward} type="submit" width="w-full" variant="primary">
+            Send A Code
+          </Button>
+        </div>
+        <div className="mt-8 h-full">
+          <ProgressBar step={1} totalSteps={3} />
+        </div>
+      </form>
+    </>
+  );
+
+  if (step === STEP.VALIDATION) {
+    bodyContent = (
+      <>
+        <form onSubmit={() => { }}>
+          <CodeInput length={5} onComplete={handleCompletedCode} />
+          <div className="flex items-center justify-between gap-x-2">
+            <Button
+              action={stepBackward}
+              type="button"
+              width="w-full"
+              variant="outline"
+            >
+              Back
+            </Button>
+            <Button
+              action={stepForward}
+              type="submit"
+              width="w-full"
+              variant="primary"
+            >
+              Verify Code
+            </Button>
+          </div>
+          <div className="mt-8 h-full">
+            <ProgressBar step={2} totalSteps={3} />
+          </div>
+        </form>
+      </>
+    );
+  }
+
+  if (step === STEP.RESETPASSWORD) {
+    bodyContent = (
+      <>
+        <form onSubmit={() => {}}>
+          <FormInput
+            id="password"
+            label="New Password"
+            isPassword
+            icon={Lock}
+            value={email}
+            onChange={(e) => setPassword(e.target.value)}
+            validationMessage="Alphanumeric characters"
+          />
+
+          <FormInput
+            id="confirmPassword"
+            label="Confirm Password"
+            isPassword
+            icon={Key}
+            value={email}
+            onChange={(e) => setConfirmPw(e.target.value)}
+            validationMessage="Re-type for password confirmation"
+          />
+          <div className="flex items-center justify-between gap-x-2">
+            <Button
+              action={stepBackward}
+              type="button"
+              width="w-full"
+              variant="outline"
+            >
+              Back
+            </Button>
+            <Button
+              action={stepForward}
+              type="submit"
+              width="w-full"
+              variant="primary"
+            >
+              Verify Code
+            </Button>
+          </div>
+          <div className="mt-8 h-full">
+            <ProgressBar step={2} totalSteps={3} />
+          </div>
+        </form>
+      </>
+    );
+  }
+
+
+  return (
+>>>>>>> c869548 (🟦 layout: reset password page)
     <AuthContainer
       title="Forgot User Password"
       caption="Reset your password by providing your user e-mail. A 5-digit authentication code would be sent to your e-mail upon request."
