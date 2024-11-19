ebaf583dbdfc2f54812426bd02f8de0043ed9942
bryancastillo10 <bryanangelo.castillo.10@gmail.com>
2024-11-19 14:34:00 +0800

    🖼️ dashboard: initial idea of dashboard charts

--- client/src/features/dashboard/Dashboard.tsx
+++ client/src/features/dashboard/Dashboard.tsx
@@ -17,16 +17,33 @@ const Dashboard = () => {
         ))}
       </div>
       <div className="grid grid-cols-1 gap-4 mt-8 sm:grid-cols-2 xl:grid-cols-3">
-        {/* Spanning two columns on larger screens */}
-        <div className="col-span-1 xl:col-span-2 h-[350px] bg-teal-500"></div>
-
-        {/* Single-column items */}
-        <div className="col-span-1 h-[350px] bg-amber-500"></div>
-        <div className="col-span-1 h-[350px] bg-sky-500"></div>
-
-        {/* Full-width on mobile, balanced columns on larger screens */}
-        <div className="col-span-1 sm:col-span-2 xl:col-span-1 h-[350px] bg-emerald-500"></div>
-        <div className="col-span-1 sm:col-span-2 xl:col-span-1 h-[350px] bg-indigo-500"></div>
+        <div className="col-span-1 xl:col-span-2 h-[350px] bg-teal-500">
+          <div className="flex flex-col odd:justify-center items-center h-full">
+            <h1 className="text-2xl">Time Series Area Chart</h1>
+            <p className="text-center">
+              pH, Temperature, DO, COD, Suspended Solids over Time
+            </p>
+          </div>
+        </div>
+        <div className="col-span-1 h-[350px] bg-amber-500">
+          <div className="flex justify-center items-center h-full">
+            <h1 className="text-2xl">Pie Chart by Water Source</h1>
+          </div>
+        </div>
+        <div className="col-span-1 sm:col-span-2 xl:col-span-1 h-[350px] bg-sky-500">
+          <div className="flex flex-col justify-center items-center h-full">
+            <h1 className="text-2xl">Radar Chart Overview</h1>
+            <p className="text-center">Comparing Parameters to Thresholds</p>
+          </div>
+        </div>
+        <div className="col-span-1 xl:col-span-2  h-[350px] bg-indigo-500">
+          <div className="flex flex-col justify-center items-center h-full">
+            <h1 className="text-2xl">Bar Chart of Nutrients</h1>
+            <p className="text-center">
+              Average Concentrations of Ammonia, Nitrates, Phosphates
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   );
