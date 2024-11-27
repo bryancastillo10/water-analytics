91f4d889f2b958ffc425f27b5c476f1106d7e5d8
bryancastillo10 <bryanangelo.castillo.10@gmail.com>
2024-11-27 15:26:25 +0800

    🎯provider: redux persistor set up

--- client/src/app/Providers.tsx
+++ client/src/app/Providers.tsx
@@ -1,16 +1,27 @@
 import { BrowserRouter } from "react-router-dom";
 import { Provider as ReduxProvider } from "react-redux";
+
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
 import store from "@/lib/redux/store";
 import CustomToastProvider from "@/app/CustomToastProvider";
 
 interface ProviderProps{
     children: React.ReactNode;
 }
+
+const persistor = persistStore(store);
+
 const Providers = ({children}:ProviderProps) => {
     return (
       <BrowserRouter future={{v7_startTransition: true, v7_relativeSplatPath: true,}}>
         <CustomToastProvider>
-          <ReduxProvider store={store}>{children}</ReduxProvider>
+          <ReduxProvider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+              {children}
+            </PersistGate>
+          </ReduxProvider>
         </CustomToastProvider>
       </BrowserRouter>
     );
