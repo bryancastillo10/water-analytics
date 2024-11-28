441b085aaaa05fce9bf3925fc0409d15da4a1612
bryancastillo10 <bryanangelo.castillo.10@gmail.com>
2024-11-28 16:48:41 +0800

    🟦layout: adjust grid spacing

--- client/src/features/user/components/AppSettings.tsx
+++ client/src/features/user/components/AppSettings.tsx
@@ -15,25 +15,26 @@ const AppSettings = () => {
   return (
     <section className="px-6 py-4">
         <TextHeader text="App Settings" />
-        <section className="grid grid-cols-2 gap-y-3">
-                <div className="flex items-center gap-2">
+        <section className="grid grid-cols-3 place-items-start gap-y-3">
+                <div className="col-span-2 flex items-center gap-2">
                     <Palette size="24"/> 
                     <h1 className="text-lg font-semibold">Theme</h1>
               </div>
               <div className="my-2">
                   <Switch isOn={theme} toggleSwitch={toggleSwitch} />
               </div>
-            <div className="flex items-center gap-2">
+            <div className="col-span-2 flex items-center gap-2">
                 <ShieldCheck size="24"/> 
                 <h1 className="text-lg font-semibold">Reset Password</h1>
               </div>
-              <div className="my-2">
+              <div className=" my-2">
                   <Button variant="primary">Reset</Button>
               </div>
-            <div className="flex items-center gap-2">
+            <div className="col-span-2 flex items-center gap-2">
                 <Bell size="24"/> 
-                <h1 className="text-lg font-semibold">Email Notifications</h1>
+                  <h1 className="text-lg font-semibold">Email Notifications</h1>
             </div>
+            
         
         </section>
     </section>
