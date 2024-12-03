9c69854a3389a0bd9ca0f15fbcfab0d11c628d34
bryancastillo10 <bryanangelo.castillo.10@gmail.com>
2024-12-03 15:55:18 +0800

    🧩 component: TextHeader can adjust font size

--- client/src/features/waterquality/components/AddMeasurementData.tsx
+++ client/src/features/waterquality/components/AddMeasurementData.tsx
@@ -1,10 +1,11 @@
 
-import { CalendarBlank } from "@phosphor-icons/react";
+import { CalendarBlank, Drop, Hexagon, Plant } from "@phosphor-icons/react";
 
 import { FormButtons } from "@/components/layout";
 import { FormInput } from "@/components/ui";
 
 import AddBasicParamsTable from "@/features/waterquality/forms/AddBasicParamsTable";
+import TextHeader from "@/components/common/TextHeader";
 
 const AddMeasurementData = () => {
   
@@ -20,8 +21,22 @@ const AddMeasurementData = () => {
           onChange={()=>{}}
           />   
       </div>
+      <div className="flex items-center gap-4 my-3">
+        <Drop size="28"/>
+        <TextHeader text="Basic Water Quality Parameter" fontSize="text-lg"/>
+      </div>
       <AddBasicParamsTable />
-    
+      <div className="flex items-center gap-4 my-3">
+        <Hexagon size="28"/>
+        <TextHeader text="Organic Pollution Indicators" fontSize="text-lg"/>
+      </div>
+      <AddBasicParamsTable />
+      <div className="flex items-center gap-4 my-3">
+        <Plant size="28"/>
+        <TextHeader text="Nutrient Polution Indicators" fontSize="text-lg"/>
+      </div>
+        <AddBasicParamsTable />
+
       <FormButtons primaryBtnLabel="Add" />
      
     </form>
