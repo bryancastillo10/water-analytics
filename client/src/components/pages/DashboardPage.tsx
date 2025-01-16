2b09d1a638e3bb93123f55c48c44b6842212f231
bryancastillo10 <bryanangelo.castillo.10@gmail.com>
2025-01-16 14:17:09 +0800

    🧩components: dashboard page set up

--- client/src/components/pages/DashboardPage.tsx
+++ client/src/components/pages/DashboardPage.tsx
@@ -1,5 +1,8 @@
 import DashboardCard from "@/features/dashboard/components/DashboardCard";
 import { statisticsCard } from "@/features/dashboard/api/mockData";
+import { NutrientsBarChart, PieChart, RadarChart, TimeSeriesLineChart } from "@/features/dashboard";
+
+
 
 const DashboardPage = () => {
   return (
@@ -17,33 +20,10 @@ const DashboardPage = () => {
         ))}
       </div>
       <div className="grid grid-cols-1 gap-4 mt-8 sm:grid-cols-2 xl:grid-cols-3">
-        <div className="col-span-1 xl:col-span-2 h-[350px] bg-teal-500">
-          <div className="flex flex-col odd:justify-center items-center h-full">
-            <h1 className="text-2xl">Time Series Area Chart</h1>
-            <p className="text-center">
-              pH, Temperature, DO, COD, Suspended Solids over Time
-            </p>
-          </div>
-        </div>
-        <div className="col-span-1 h-[350px] bg-amber-500">
-          <div className="flex justify-center items-center h-full">
-            <h1 className="text-2xl">Pie Chart by Water Source</h1>
-          </div>
-        </div>
-        <div className="col-span-1 sm:col-span-2 xl:col-span-1 h-[350px] bg-sky-500">
-          <div className="flex flex-col justify-center items-center h-full">
-            <h1 className="text-2xl">Radar Chart Overview</h1>
-            <p className="text-center">Comparing Parameters to Thresholds</p>
-          </div>
-        </div>
-        <div className="col-span-1 xl:col-span-2  h-[350px] bg-indigo-500">
-          <div className="flex flex-col justify-center items-center h-full">
-            <h1 className="text-2xl">Bar Chart of Nutrients</h1>
-            <p className="text-center">
-              Average Concentrations of Ammonia, Nitrates, Phosphates
-            </p>
-          </div>
-        </div>
+        <TimeSeriesLineChart />
+        <PieChart />
+        <RadarChart />
+        <NutrientsBarChart/>
       </div>
     </main>
   );
---
+++ client/src/features/dashboard/components/RadarChart.tsx
@@ -0,0 +1,12 @@
+const RadarChart = () => {
+  return (
+    <div className="col-span-1 sm:col-span-2 xl:col-span-1 h-[350px] bg-sky-500">
+      <div className="flex flex-col justify-center items-center h-full">
+        <h1 className="text-2xl">Radar Chart Overview</h1>
+        <p className="text-center">Comparing Parameters to Thresholds</p>
+      </div>
+    </div>
+  )
+}
+
+export default RadarChart
