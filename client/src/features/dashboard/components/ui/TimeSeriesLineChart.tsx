8132a0c9c7a3a99c082ed5069cd12ea30ae54dee
bryancastillo10 <bryan.angelocastillo.10@gmail.com>
2025-01-28 08:14:19 +0800

    📈 area to line chart

--- client/src/features/dashboard/components/ui/TimeSeriesLineChart.tsx
+++ client/src/features/dashboard/components/ui/TimeSeriesLineChart.tsx
@@ -1,4 +1,5 @@
-import { ResponsiveContainer, AreaChart, CartesianGrid, XAxis, YAxis, Area, Tooltip } from "recharts";
+import { ResponsiveContainer, LineChart, CartesianGrid, XAxis, YAxis, Line, Tooltip } from "recharts";
+import { useAppSelector } from "@/lib/redux/hooks";
 
 interface TimeSeriesLineChartProps{
    selectedParameter: string;
@@ -9,9 +10,35 @@ const TimeSeriesLineChart = ({
   timeSeriesData,
   selectedParameter
 }: TimeSeriesLineChartProps) => {
+  const theme = useAppSelector((state) => state.theme.isDarkMode);
+
+  const renderTooltip = ({ active, payload, label }: any) => {
+    if (active && payload && payload.length) {
+      return (
+        <div
+          className="border border-neutral rounded-md shaodw-md p-2"
+          style={{
+            backgroundColor: theme ? "#545454" : "#F4F3F2",
+            color: theme ? "#ffffff" : "#040710",
+          }}
+        >
+          <p className="text-sm">{`Date: ${label}`}</p>
+          <p
+            style={{
+              color: theme ? "#13b6f6" : "#006da3",
+            }}
+          >
+            {`${selectedParameter}: ${payload[0].value}`}
+          </p>
+        </div>
+      );
+    }
+    return null;
+  };
+
   return (    
        <ResponsiveContainer className="pt-4" width="100%" height="90%">
-        <AreaChart
+        <LineChart
           data={timeSeriesData}
           margin={{
             top: 10,
@@ -23,10 +50,10 @@ const TimeSeriesLineChart = ({
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis 
             dataKey="date"
-            tick={{ fontSize: 12 }}
+            tick={{ fontSize: 12, color: theme ? "#ffffff" : "#040710" }}
           />
           <YAxis
-            tick={{ fontSize: 12 }}
+            tick={{ fontSize: 12, color: theme ? "#ffffff" : "#040710" }}
             label={{ 
               value: selectedParameter, 
               angle: -90, 
@@ -34,24 +61,17 @@ const TimeSeriesLineChart = ({
               style: { textAnchor: 'middle' }
             }}
           />
-          <Tooltip
-            contentStyle={{
-              backgroundColor: '#F4F3F2',
-              border: '1px solid #c2c2c2',
-              color:"#040710",
-              borderRadius: '4px',
-            }}
-          />
-          <Area
+          <Tooltip content={renderTooltip} />
+          <Line
             type="monotone"
             dataKey="value"
             stroke="#13b6f6"
             fill="#006da3"
-            fillOpacity={0.3}
+            fillOpacity={0.5}
             dot={{ r: 2 }}
             name={selectedParameter}
           />
-        </AreaChart>
+        </LineChart>
       </ResponsiveContainer>
   )
 }
