cca8bc1079dd175bb6513a19a2bf72e116a53521
bryancastillo10 <bryan.angelocastillo.10@gmail.com>
2025-01-29 11:10:38 +0800

    🟦 loading state: dashboard card

--- client/src/features/dashboard/components/sections/DashboardCardBlock.tsx
+++ client/src/features/dashboard/components/sections/DashboardCardBlock.tsx
@@ -5,7 +5,7 @@ import { parameterIcons } from "@/features/dashboard/utils/parameterMapping";
 const DashboardCardBlock = () => {
   const selectedSiteId = useAppSelector((state) => state.dashboard.selectedSiteId);
   const siteId = selectedSiteId || "";
-  const {data: statistics } = useGetDashboardCardValuesQuery(siteId);
+  const {data: statistics, isLoading } = useGetDashboardCardValuesQuery(siteId);
   
     const statisticsCard = statistics?.map((stat) => ({
         parameter: stat.parameter,
@@ -14,6 +14,20 @@ const DashboardCardBlock = () => {
         status: stat.status || "Unknown",
         icon: parameterIcons[stat.parameter] || null
     }));
+
+    if (isLoading) {
+        return (
+            <div className="grid gap-y-4 grid-cols-1 mt-4 md:grid-cols-2 xl:grid-cols-4 sm:place-items-center xl:place-items-start gap-8">
+            {Array.from({length:4}).map((_, index) => (
+                <div
+                    key={index}
+                    className="w-[250px] h-[190px] overflow-hidden max-w-md 
+                    bg-gradient-to-br from-secondary to-primary rounded-xl
+                    shadow-lg p-4 lg:p-6 transition-all duration-300 hover:shadow-xl
+                    relative animate-pulse"/>
+            ))}
+            </div>
+        )}
 
     console.log(statisticsCard);
     return (
