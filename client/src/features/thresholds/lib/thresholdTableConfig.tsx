c9693637e07f6203a2c5852fa65505c45880037b
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2025-02-16 08:58:33 +0800

    🟦 layout: threshold table

--- client/src/features/thresholds/lib/thresholdTableConfig.tsx
+++ client/src/features/thresholds/lib/thresholdTableConfig.tsx
@@ -1,13 +1,20 @@
 import { createColumnHelper } from "@tanstack/react-table";
 import { type IThresholdData } from "@/features/thresholds/api/interface";
+import { parameterRecord } from "@/lib/mappings/parameterRecord";
 
 const columnHelper = createColumnHelper<IThresholdData>();
 
 export const thresholdColumns = [
-  columnHelper.accessor("parameter", {
-    header: () => "Parameter",
-    cell: (info) => info.getValue(),
-  }),
+ columnHelper.accessor("parameter", {
+     header: () => "Parameter",
+     cell: (info) => {
+       const parameterInfo = info.getValue();
+       const parameterLabel = Object.entries(parameterRecord).find(
+         ([_, mappedParam]) => mappedParam === parameterInfo
+       )?.[0] || parameterInfo;
+       return parameterLabel;
+     }
+   }),
   columnHelper.accessor("unit", {
     header: () => "Unit",
     cell: (info) => info.getValue(),
@@ -15,10 +22,16 @@ export const thresholdColumns = [
   columnHelper.accessor((row) => {
     if (row.parameter === "pH") {
       return (
-        <div className="grid grid-cols-2 justify-between">
-          <p>Min: {row.minValue}</p>
-          <p>Max: {row.maxValue}</p>
+        <div className="flex flex-col">
+        <div className="flex items-center gap-2">
+          <span className="font-semibold">Min:</span>
+          <span className="">{row.minValue}</span>
         </div>
+        <div className="flex items-center gap-2 mt-1">
+          <span className="font-semibold">Max:</span>
+          <span className="">{row.maxValue}</span>
+        </div>
+      </div>
     );
     }
     return row.value;
