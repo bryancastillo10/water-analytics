c6a179722477766b163f1d3a187253f89f53eda6
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2025-02-18 09:48:21 +0800

    📁 structure: move gitignore & adjust server listening for deployment

--- server/index.ts
+++ server/index.ts
@@ -9,9 +9,9 @@ const initializeServer = async () => {
         await connectToDb();
 
         const app = startApp();
-        const port = process.env.PORT
+        const port = parseInt(process.env.PORT || "3000");
 
-        app.listen(port, () => {
+        app.listen(port, '0.0.0.0', () => {
             console.log(`Server is running on port ${port}`);
         });
     } catch (error) {
